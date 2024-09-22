b0d792de3842ab2025d707907ffed75721448efa
Harsh Raj <samarshelley@gmail.com>
2024-09-23 01:51:35 +0530

    filesUploaded

---
+++ quest.js
@@ -0,0 +1,142 @@
+const questions = [
+    {
+        question: "Best college of AKTU ?",
+         answers: [
+            {text: "AKG" , correct: "true"},
+            {text: "ABES" , correct: "false"},
+            {text: "KNIT" , correct: "false"},
+            {text: "IET" , correct: "false"},
+
+
+         ]
+
+
+    },
+    {
+        question: "Which branch is best ?",
+         answers: [
+            {text: "AIML(ye course ha, branch nhi)" , correct: "false"},
+            {text: "CS" , correct: "true"},
+            {text: "IT" , correct: "false"},
+            {text: "ME" , correct: "false"},
+
+
+         ]
+    },
+    {
+        question: "Gaurav is best for DSA?",
+         answers: [
+            {text: "YES" , correct: "false"},
+            {text: "NO" , correct: "true"},
+           
+
+
+         ]
+    },
+    {
+        question: "Hosteller ?",
+         answers: [
+            {text: "YES" , correct: "true"},
+            {text: "NO" , correct: "false"},
+            
+
+
+         ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    
+    currentQuestionIndex = 0;
+    score=0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+        resetState();
+        let currentQuestion = questions[currentQuestionIndex];  
+        let questionNo = currentQuestionIndex + 1;
+        questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+
+        
+    });
+
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct ==="true";
+
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+
+    } );
+    nextButton.style.display = "block"; 
+}
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML= "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }
+
+    else{
+        showScore();    
+    }
+
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+
+});
+startQuiz();
